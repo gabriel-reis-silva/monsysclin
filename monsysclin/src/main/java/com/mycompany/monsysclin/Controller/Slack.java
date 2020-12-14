1d92fdd2e356d88bde10f2560343f90b25b74fa8
Gabriel Augusto Reis <61751731+gabriel-reis-silva@users.noreply.github.com>
2020-12-14 00:54:18 -0300

    bot novo do slack e envio de alertas no slack

---
+++ monsysclin/src/main/java/com/mycompany/monsysclin/Controller/Slack.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.monsysclin.Controller;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author gabsg
+ */
+public class Slack {
+
+    private String url = "https://hooks.slack.com/services/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";
+
+    public void sendMessage(JSONObject message) throws Exception {
+
+        URL obj = new URL(this.url);
+
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(message.toString());
+
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+
+        System.out.println("Sending 'POST' request to URL: " + this.url);
+        System.out.println("POST parameters: " + message.toString());
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = reader.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        reader.close();
+        System.out.println("Success.");
+    }
+}
